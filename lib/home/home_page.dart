2d49ddc0dec679fcdae3fba4e4bb0a43defa1b01
Fernando Andrade <fernandofreamunde@gmail.com>
2021-04-20 19:46:10 +0200

    add some padding and some quiz cards to homepage

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/home/widgets/app_bar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,14 +16,31 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBarWidget(),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        child: Column(
           children: [
-            LevelButtonWidget(label: "Facil"),
-            LevelButtonWidget(label: "Medio"),
-            LevelButtonWidget(label: "Dificil"),
-            LevelButtonWidget(label: "Perito"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: "Facil"),
+                LevelButtonWidget(label: "Medio"),
+                LevelButtonWidget(label: "Dificil"),
+                LevelButtonWidget(label: "Perito"),
+              ],
+            ),
+            SizedBox(height:24),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2, 
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+              ])
+            )
           ],
         ),
       ),
